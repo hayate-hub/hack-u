1491fe87b57413ab841402d649e91f3dfe311ef9
Hayate Fukumoto <hfuwtanks@gmail.com>
2020-07-12 11:02:55 +0900

    本番前環境commit

--- app/sample.py
+++ app/sample.py
@@ -3,14 +3,5 @@ import os
 import subprocess
 import time
 
-
-for i in range(100):
-        time.sleep(3)
-        f = open("pattern_out.txt")
-        pattern = f.read()
-        print("processing...")
-        print("pattern:" + pattern)
-        if int(pattern) == 1 :
-            break
-
-voice_changer.doing_all()
+def doing_vc():
+    voice_changer.doing_all()
